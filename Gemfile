03e6c3a3116aa70bfa878033c6c56a79711a0d81
Klashe <officialklashed@gmail.com>
2020-10-20 16:14:12 +0100

    Finish working and refactored palindrome method

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source "https://rubygems.org"
 
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
 # Specify your gem's dependencies in tolesh_palindrome.gemspec
 gemspec
+
+gem 'minitest-reporters', '1.2.0'
--- lib/tolesh_palindrome.rb
+++ lib/tolesh_palindrome.rb
@@ -1,6 +1,19 @@
 require "tolesh_palindrome/version"
 
-module ToleshPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # def letters
+  #   self.chars.select { |c| c.match(/[a-z]/i) }.join
+  # end
+  
+  private
+  
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
 end
+
